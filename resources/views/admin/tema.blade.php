29604adf14c5a5b2c820941662ab367dc8170def
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-06-01 10:19:11 +0300

    2

--- resources/views/admin/tema.blade.php
+++ resources/views/admin/tema.blade.php
@@ -7,14 +7,14 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin')}}/images/favicon.png">
     <!-- Pignose Calender -->
-    <link href="plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    <link href="{{asset('admin')}}/plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
     <!-- Chartist -->
-    <link rel="stylesheet" href="plugins/chartist/css/chartist.min.css">
-    <link rel="stylesheet" href="plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+    <link rel="stylesheet" href="{{asset('admin')}}/plugins/chartist/css/chartist.min.css">
+    <link rel="stylesheet" href="{{asset('admin')}}/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
     <!-- Custom Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{asset('admin')}}/css/style.css" rel="stylesheet">
 
 </head>
 
@@ -46,10 +46,10 @@
         <div class="nav-header">
             <div class="brand-logo">
                 <a href="index.html">
-                    <b class="logo-abbr"><img src="images/logo.png" alt=""> </b>
-                    <span class="logo-compact"><img src="images/logo-compact.png" alt=""></span>
+                    <b class="logo-abbr"><img src="{{asset('admin')}}/images/logo.png" alt=""> </b>
+                    <span class="logo-compact"><img src="{{asset('admin')}}/images/logo-compact.png" alt=""></span>
                     <span class="brand-title">
-                        <img src="images/logo-text.png" alt="">
+                        <img src="{{asset('admin')}}/images/logo-text.png" alt="">
                     </span>
                 </a>
             </div>
@@ -977,33 +977,33 @@
     <!--**********************************
         Scripts
     ***********************************-->
-    <script src="plugins/common/common.min.js"></script>
-    <script src="js/custom.min.js"></script>
-    <script src="js/settings.js"></script>
-    <script src="js/gleek.js"></script>
-    <script src="js/styleSwitcher.js"></script>
+    <script src="{{asset('admin')}}/plugins/common/common.min.js"></script>
+    <script src="{{asset('admin')}}/js/custom.min.js"></script>
+    <script src="{{asset('admin')}}/js/settings.js"></script>
+    <script src="{{asset('admin')}}/js/gleek.js"></script>
+    <script src="{{asset('admin')}}/js/styleSwitcher.js"></script>
 
     <!-- Chartjs -->
-    <script src="plugins/chart.js/Chart.bundle.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/chart.js/Chart.bundle.min.js"></script>
     <!-- Circle progress -->
-    <script src="plugins/circle-progress/circle-progress.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/circle-progress/circle-progress.min.js"></script>
     <!-- Datamap -->
-    <script src="plugins/d3v3/index.js"></script>
-    <script src="plugins/topojson/topojson.min.js"></script>
-    <script src="plugins/datamaps/datamaps.world.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/d3v3/index.js"></script>
+    <script src="{{asset('admin')}}/plugins/topojson/topojson.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/datamaps/datamaps.world.min.js"></script>
     <!-- Morrisjs -->
-    <script src="plugins/raphael/raphael.min.js"></script>
-    <script src="plugins/morris/morris.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/raphael/raphael.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/morris/morris.min.js"></script>
     <!-- Pignose Calender -->
-    <script src="plugins/moment/moment.min.js"></script>
-    <script src="plugins/pg-calendar/js/pignose.calendar.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/moment/moment.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/pg-calendar/js/pignose.calendar.min.js"></script>
     <!-- ChartistJS -->
-    <script src="plugins/chartist/js/chartist.min.js"></script>
-    <script src="plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/chartist/js/chartist.min.js"></script>
+    <script src="{{asset('admin')}}/plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js"></script>
 
 
 
-    <script src="js/dashboard/dashboard-1.js"></script>
+    <script src="{{asset('admin')}}/js/dashboard/dashboard-1.js"></script>
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/yonetim', function () {
     return view('admin.tema');
 });
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+});
