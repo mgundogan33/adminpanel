258dcbae8143b8d34854802f9068c76c52d411fd
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-06-01 11:22:07 +0300

    3

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/yonetim', function () {
-    return view('admin.tema');
+    return view('admin.include.home');
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
