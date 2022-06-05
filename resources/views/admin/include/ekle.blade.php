8cef4936de6d39c0d8c1981b1fbe96862280c6a3
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-06-05 15:42:08 +0300

    9

--- resources/views/admin/include/ekle.blade.php
+++ resources/views/admin/include/ekle.blade.php
@@ -11,30 +11,39 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
+                    @if (session('basarili'))
+                        <div class="alert alert-success">{{ session('basarili') }}</div>
+                    @endif
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">{{ $modulBilgisi->baslik }}</h4>
-                            <p class="text-muted m-b-15 f-s-12">Use the input classes on an <code>.input-default, input-flat,
-                                    .input-rounded</code> for Default input.</p>
                             <div class="basic-form">
-                                <form action="" method="POST">
+                                <form action="{{ route('eklepost', $modulBilgisi->seflink) }}" method="POST"
+                                    enctype="multipart/form-data">
                                     @csrf
                                     <div class="form-group">
                                         <label>Kategori</label>
                                         <select class="form-control input-default" name="kategori">
-
+                                            @if ($kategoriBilgisi)
+                                                @foreach ($kategoriBilgisi as $kategori)
+                                                    <option value="{{ $kategori->id }}">
+                                                        {{ stripslashes($kategori->baslik) }}</option>
+                                                @endforeach
+                                            @endif
                                         </select>
                                     </div>
                                     <div class="form-group">
                                         <label>Başlık</label>
-                                        <input type="text" class="form-control input-default" placeholder="Başlık"
+                                        <input type="text" class="form-control input-default" placeholder="Başlık" required
                                             name="baslik">
                                     </div>
-                                    <div class="summernote">
+                                    <div class="form-group">
+                                        <label>Açıklama</label>
+                                        <textarea name="metin" class="summernote"></textarea>
                                     </div>
                                     <div class="form-group">
                                         <label>Resim</label>
-                                        <input type="text" class="form-control input-default" placeholder="Resim Seçiniz"
+                                        <input type="file" class="form-control input-default" placeholder="Resim"
                                             name="resim">
                                     </div>
                                     <div class="form-group">
@@ -46,6 +55,14 @@
                                         <label>Description</label>
                                         <input type="text" class="form-control input-default" placeholder="Description"
                                             name="description">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Sıra No</label>
+                                        <input type="number" class="form-control input-default" placeholder="Sıra No"
+                                            name="sirano" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary" name="gonder" value="Kaydet" required>
                                     </div>
                                 </form>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/yonetim/moduller',[ModulController::class,'index'])->name('moduller
 Route::post('/yonetim/modul-ekle',[ModulController::class,'modulekle'])->name('modul-ekle');
 Route::get('/yonetim/liste/{modul}',[AdminYonetim::class,'liste'])->name('liste');
 Route::get('/yonetim/ekle/{modul}',[AdminYonetim::class,'ekle'])->name('ekle');
+Route::post('/yonetim/ekle-post/{modul}',[AdminYonetim::class,'eklePost'])->name('eklepost');
