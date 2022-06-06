d55959e104ccf76e9198dde596660f1b7f119944
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-06-06 17:34:12 +0300

    15

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminYonetim;
 use App\Http\Controllers\ModulController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,18 +11,19 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
+Route::middleware(['auth:sanctum','verified'])->prefix('yonetim')->group(function(){
+    Route::get('/',[AdminYonetim::class,'home'])->name('home');
+    Route::get('/moduller',[ModulController::class,'index'])->name('moduller');
+    Route::post('/modul-ekle',[ModulController::class,'modulekle'])->name('modul-ekle');
+    Route::get('/liste/{modul}',[AdminYonetim::class,'liste'])->name('liste');
+    Route::get('/ekle/{modul}',[AdminYonetim::class,'ekle'])->name('ekle');
+    Route::post('/ekle-post/{modul}',[AdminYonetim::class,'eklePost'])->name('eklepost');
+    Route::get('/duzenle/{modul}/{id}',[AdminYonetim::class,'duzenle'])->name('duzenle');
+    Route::post('/duzenle-post/{modul}/{id}',[AdminYonetim::class,'duzenlePost'])->name('duzenlepost');
+    Route::get('/sil/{modul}/{id}',[AdminYonetim::class,'sil'])->name('sil');
+    Route::get('/durum/{modul}/{id}',[AdminYonetim::class,'durum'])->name('durum');
 
-Route::get('/yonetim',[AdminYonetim::class,'home'])->name('home');
-Route::get('/yonetim/moduller',[ModulController::class,'index'])->name('moduller');
-Route::post('/yonetim/modul-ekle',[ModulController::class,'modulekle'])->name('modul-ekle');
-Route::get('/yonetim/liste/{modul}',[AdminYonetim::class,'liste'])->name('liste');
-Route::get('/yonetim/ekle/{modul}',[AdminYonetim::class,'ekle'])->name('ekle');
-Route::post('/yonetim/ekle-post/{modul}',[AdminYonetim::class,'eklePost'])->name('eklepost');
-Route::get('/yonetim/duzenle/{modul}/{id}',[AdminYonetim::class,'duzenle'])->name('duzenle');
-Route::post('/yonetim/duzenle-post/{modul}/{id}',[AdminYonetim::class,'duzenlePost'])->name('duzenlepost');
-Route::get('/yonetim/sil/{modul}/{id}',[AdminYonetim::class,'sil'])->name('sil');
-Route::get('/yonetim/durum/{modul}/{id}',[AdminYonetim::class,'durum'])->name('durum');
+    Route::get('/ayarlar',[AdminYonetim::class,'ayarlar'])->name('ayarlar');
+    Route::post('/ayarpost',[AdminYonetim::class,'ayarpost'])->name('ayarpost');
 
-
-Route::get('/yonetim/ayarlar',[AdminYonetim::class,'ayarlar'])->name('ayarlar');
-Route::post('/yonetim/ayarpost',[AdminYonetim::class,'ayarpost'])->name('ayarpost');
+});
